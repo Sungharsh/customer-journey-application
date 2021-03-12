13555d1c12252a87a3084388521fe416040bec3d
sungharsh <suncam123@gmail.com>
2021-03-12 23:23:23 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Customer from './Customer'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Customer />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/servayData.js
@@ -0,0 +1,40 @@
+const questions = [
+  {
+    questionText: 'What model smart phone did you buy today?',
+    answerOptions: [
+      { answerText: 'Apple Iphone', isCorrect: true },
+      { answerText: 'Samsung Galaxy', isCorrect: true },
+      { answerText: 'Google Pixel', isCorrect: true },
+      { answerText: 'Nokia', isCorrect: true },
+    ],
+  },
+  {
+    questionText: 'How many of them did you buy?',
+    answerOptions: [
+      { answerText: 'One', isCorrect: true },
+      { answerText: 'Two', isCorrect: true },
+      { answerText: 'Three', isCorrect: true },
+      { answerText: 'Four', isCorrect: true },
+    ],
+  },
+  {
+    questionText: 'How much did the product cost?',
+    answerOptions: [
+      { answerText: 'Below £500', isCorrect: true },
+      { answerText: 'Above £500', isCorrect: true },
+      { answerText: 'Below £1000', isCorrect: true },
+      { answerText: 'Above £1000', isCorrect: true },
+    ],
+  },
+  {
+    questionText: 'How is your overall buying experience ?',
+    answerOptions: [
+      { answerText: 'Poor', isCorrect: true },
+      { answerText: 'Average', isCorrect: true },
+      { answerText: 'Good', isCorrect: true },
+      { answerText: 'Excellent', isCorrect: true },
+    ],
+  },
+]
+
+export default questions
