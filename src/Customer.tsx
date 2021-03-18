6e9983c91404087db3cfdb6d9e5f306819aff4a0
sungharsh <suncam123@gmail.com>
2021-03-18 21:14:07 +0000

    code prettify, auto formate did not work on previous commit

--- src/Customer.tsx
+++ src/Customer.tsx
@@ -3,59 +3,71 @@ import QUESTIONS from './surveyData'
 import './App.css'
 
 export default function App() {
-	const questions = QUESTIONS
-	const [currentQuestion, setCurrentQuestion] = useState(0)
-	const [showScore, setShowScore] = useState(false)
-	const [total, setTotal] = useState<number>(0)
+  const questions = QUESTIONS
+  const [currentQuestion, setCurrentQuestion] = useState(0)
+  const [showScore, setShowScore] = useState(false)
+  const [total, setTotal] = useState<number>(0)
   const [answerGroup, setAnswerGroup] = useState<Array<string>>([])
 
-	const handleAnswerOptionClick = (answerOption: { answerText?: string; isCorrect: boolean }) => {
-		if (answerOption.isCorrect) {
-			setTotal(total + 1);
-		}
+  const handleAnswerOptionClick = (answerOption: {
+    answerText?: string
+    isCorrect: boolean
+  }) => {
+    if (answerOption.isCorrect) {
+      setTotal(total + 1)
+    }
     if (answerOption.answerText) {
       const answer = answerOption.answerText
-      setAnswerGroup((answerGroup:string[]) => [...answerGroup, answer])
+      setAnswerGroup((answerGroup: string[]) => [...answerGroup, answer])
     }
-		const nextQuestion = currentQuestion + 1;
-		if (nextQuestion < questions.length) {
-			setCurrentQuestion(nextQuestion);
-		} else {
-			setShowScore(true);
-		}
-	};
+    const nextQuestion = currentQuestion + 1
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion)
+    } else {
+      setShowScore(true)
+    }
+  }
 
-	return (
-		<div className='customerWrapper'>
-			{showScore ? (
-				<div className='score-section'>
-          <h3>You answered {total} out of {questions.length}</h3>
+  return (
+    <div className='customerWrapper'>
+      {showScore ? (
+        <div className='score-section'>
+          <h3>
+            You answered {total} out of {questions.length}
+          </h3>
           <ul>
             {answerGroup.map((item, index) => {
               return <li key={index}>{item}</li>
             })}
           </ul>
-				</div>
-			) : (
-				<>
-					<div className='question-section'>
+        </div>
+      ) : (
+        <>
+          <div className='question-section'>
             <h2>Customer journey servay</h2>
-						<div className='question-count'>
-							<span>Question {currentQuestion + 1}</span>/{questions.length}
-						</div>
-						<div className='question-text'>{questions[currentQuestion].questionText}</div>
-					</div>
-					<div className='answer-section'>
-						{questions[currentQuestion].answerOptions.map((answerOption, index) => (
-              
-							<button key={index} 
-              onClick={() => { handleAnswerOptionClick(answerOption)            
-            }}
-              >{answerOption.answerText}</button> 
-						))}
-					</div>
-				</>
-			)}
-		</div>
-	);
+            <div className='question-count'>
+              <span>Question {currentQuestion + 1}</span>/{questions.length}
+            </div>
+            <div className='question-text'>
+              {questions[currentQuestion].questionText}
+            </div>
+          </div>
+          <div className='answer-section'>
+            {questions[currentQuestion].answerOptions.map(
+              (answerOption, index) => (
+                <button
+                  key={index}
+                  onClick={() => {
+                    handleAnswerOptionClick(answerOption)
+                  }}
+                >
+                  {answerOption.answerText}
+                </button>
+              )
+            )}
+          </div>
+        </>
+      )}
+    </div>
+  )
 }
